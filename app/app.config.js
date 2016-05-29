c706a87932172103d69a2268f35438215495566f
Oleksiy <oleksiypolshchak@gmail.com>
2016-05-29 21:14:27 +0300

    prepare

--- app/app.config.js
+++ app/app.config.js
@@ -1,1 +1,17 @@
 //app.config.js
+angular.
+  module('myApp').
+  config(['$locationProvider' ,'$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/main', {
+          template: '<profile></profile>'
+        }).
+        when('/main/profile', {
+          template: '<costumer></costumer>'
+        }).
+        otherwise('/main');
+    }
+  ]);

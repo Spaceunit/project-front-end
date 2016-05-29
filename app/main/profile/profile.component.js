3c8f207eb11c7d59acd96e6dbff802886a199fa2
Oleksiy <oleksiypolshchak@gmail.com>
2016-05-29 21:20:05 +0300

    for Govan

--- app/main/profile/profile.component.js
+++ app/main/profile/profile.component.js
@@ -1,1 +1,14 @@
 //profile.component.js
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('profile').
+  component('profile', {
+    templateUrl: 'main/profile/profile.template.html',
+    controller: ['profile',
+      function ProfileController(Role) {
+        //this.some = 0
+      }
+    ]
+  });

4e330736dc037c5c108872ebaced99ace902d87a
Oleksiy <oleksiypolshchak@gmail.com>
2016-05-30 12:56:10 +0300

    work)

--- app/main/main.component.js
+++ app/main/main.component.js
@@ -1,15 +1,14 @@
 //main.component.js
 'use strict';
-
 // Register `phoneList` component, along with its associated controller and template
 angular.
   module('main').
   component('main', {
     templateUrl: 'main/main.template.html',
-    controller: ['Main',
+    controller: [
       function MainController(Main) {
         //some do
-        var.self = this;
+        this.lol = 2;
       }
     ]
   });

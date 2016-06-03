3731c5e24805df640a9a0605d200fc6daa5a3450
Oleksiy <oleksiypolshchak@gmail.com>
2016-06-04 00:33:11 +0300

    carrier content

--- app/main/profile/profile.component.js
+++ app/main/profile/profile.component.js
@@ -9,9 +9,9 @@ angular.
       function ProfileController($scope) {
         //this.some = 0
         //adding a role
-        this.imageURL = 'media/photo.jpg';
-        this.height = 300;
-        this.width  = 200;
+        this.imageURL = 'media/ic_add_a_photo_black_48px.svg';
+        this.height = '';
+        this.width  = '300px';
         this.name = 'Lol'
         $scope.addRole = function addRole(id){
           id = 0;

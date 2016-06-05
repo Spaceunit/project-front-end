2a42a3e17eb9e07b643219b888b94b23bc344240
Vavadimir <freesoulvova@gmail.com>
2016-06-05 15:57:02 +0300

    vendor test

---
+++ app/main/profile/vendor/vendor.component.js
@@ -0,0 +1,13 @@
+//vendor.component.js
+'use strict';
+
+angular.
+module('vendor').
+component('vendor', {
+    templateUrl: 'main/profile/vendor/vendor.vendor.html',
+    controller: ['$scope',
+        function СarrierController($scope) {
+            //this.some = 0
+        }
+    ]
+});
